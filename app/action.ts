bfc9d3bd725306fe6befab33d8072132036f97b3
sahil-gusain <sahilgusainddn@gmail.com>
2024-01-21 23:01:48 +0530

    solved image issue

--- app/action.ts
+++ app/action.ts
@@ -1,5 +1,5 @@
-export async function fetchAnime(){
-    const response = await fetch('https://shikimori.one/api/animes?pages=1');
+export async function fetchAnime(pages:number){
+    const response = await fetch(`https://shikimori.one/api/animes?pages=${pages}&limit=12`);
     
     const data = await response.json();
     console.log(data);
