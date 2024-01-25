23f42e44e2ecd64d4758ffce3ef6eacde70efb20
sahil-gusain <sahilgusainddn@gmail.com>
2024-01-25 12:11:18 +0530

    finished the frame

--- app/action.ts
+++
@@ -1,7 +0,0 @@
-export async function fetchAnime(pages:number){
-    const response = await fetch(`https://shikimori.one/api/animes?pages=${pages}&limit=12`);
-    
-    const data = await response.json();
-    console.log(data);
-    return data;
-}
